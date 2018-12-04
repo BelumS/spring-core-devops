18d2a4f2a8410551be2997b62d5b3562818d5a2c
belum <beldsahn@gmail.com>
2018-12-04 13:06:06 -0600

    testing

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -10,6 +10,7 @@ public class SpringCoreDevOpsApplication {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
 		//this is a whitespace change test
+		//TEST
 		// another test...
 	}
 }
